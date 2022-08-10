eac661409f79f2d529949e49888712aa19afccd9
Brandon Davis <BUDavis@student.fullsail.edu>
2022-08-09 21:40:16 -0400

    Game manager

---
+++ FantasticStarsPPII/Assets/Scripts/gameManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+
+    public GameObject player;
+    public playerController playerScript;
+
+    public GameObject pauseMenu;
+    public GameObject currentMenuOpen;
+
+    public GameObject playerDamageFlash;
+    public GameObject playerDeadMenu;
+
+
+    public bool isPaused;
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<playerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            isPaused = !isPaused;
+            currentMenuOpen = pauseMenu;
+            currentMenuOpen.SetActive(isPaused);
+
+            if (isPaused)
+                CursorLock();
+            else if (currentMenuOpen)
+                CursorUnlock();
+        }
+    }
+
+    public void CursorLock()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+        Time.timeScale = 0;
+    }
+
+    public void CursorUnlock()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1;
+        currentMenuOpen.SetActive(isPaused);
+        currentMenuOpen = null;
+    }
+}
